45dfdedf0ab77eb5f1702ecb77f71a97d44fb52b
daeheon_macbook <neohgeek@gmail.com>
2022-09-22 16:10:54 +0900

    feat: Create media stream from inject.js
    The inject.js now create media stream from target tab.
    It opens handler.html.
    It controls media stream status based on the background.js message. (either on or off)

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,62 @@
-const startStream = () => {
+// const getSize = () => {
+//   const h = window.innerHeight;
+//   const w = window.innerWidth;
+//   return { h, w };
+// };
+
+let capturedStream = null;
+let handlerTab = null;
+
+// const startStream = () => {
+//   const { h, w } = getSize();
+//   const handlerTab = null;
+
+//   chrome.tabCapture.capture(
+//     {
+//       audio: false,
+//       video: true,
+//       videoConstraints: {},
+//     },
+//     (mediaStream) => {
+//       sendStreamToHandler(mediaStream, handlerTab);
+//     }
+//   );
+//   handlerTab = window.open("./src/handler/handler.html");
+//   chrome.tabs.create({
+//     url: chrome.extension.getURL("./src/handler/handler.html"),
+//     selected: true,
+//   });
+// };
+
+const startStream = async () => {
   const url = chrome.runtime.getURL("./src/handler/handler.html");
-  const handlerTab = window.open(url);
-  console.log(handlerTab);
+  capturedStream = await navigator.mediaDevices.getDisplayMedia({
+    video: true,
+    audio: false,
+  });
+  sendStreamToHandler(capturedStream, url);
+};
+
+const sendStreamToHandler = (capturedStream, url) => {
+  if (capturedStream == null) {
+    console.error("Error starting tab capture");
+    return;
+  }
+  if (handlerTab != null) {
+    handlerTab.close();
+  }
+  handlerTab = window.open(url);
+  // handlerTab.currentStream = capturedStream;
+};
+
+const stopStream = () => {
+  capturedStream = null;
+  handlerTab.close();
+  handlerTab = null;
 };
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   switch (message.key) {
-    case "notify":
-      console.log("injected");
     case "check":
       sendResponse({ received: true });
       break;
