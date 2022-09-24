0cbd5989a88c4d777a6d2aa1000c4ffc8ddd0254
YongHyunKing <99469068+YongHyunKing@users.noreply.github.com>
2022-09-25 00:44:51 +0900

    test1

--- src/handler/handler.js
+++ src/handler/handler.js
@@ -1,3 +1,4 @@
+//test1
 var h;
 var w;
 var timer;
@@ -9,9 +10,9 @@ const vol = document.getElementById("vol");
 
 const readLocalStorage = async (key) => {
   return new Promise((resolve, reject) => {
-  	chrome.storage.local.get([key], function(result) {
-    	if (result[key] == undefined){
-      	reject();
+    chrome.storage.local.get([key], function (result) {
+      if (result[key] == undefined) {
+        reject();
       } else {
         resolve(result[key]);
       }
@@ -24,38 +25,38 @@ const edgeDetection = (player, streamCanvas) => {
   streamCanvas.style.display = "none";
 
   screenInterval = setInterval(() => {
-		var canvasContext = streamCanvas.getContext("2d");
-		var src = cv.imread("streamCanvas");
-		let ksize = new cv.Size(10, 10);
-		let anchor = new cv.Point(-1, -1);
+    var canvasContext = streamCanvas.getContext("2d");
+    var src = cv.imread("streamCanvas");
+    let ksize = new cv.Size(10, 10);
+    let anchor = new cv.Point(-1, -1);
 
-		canvasContext.drawImage(
-			player, 
-			(streamCanvas.width - player.videoWidth) / 2, 
-			(streamCanvas.height - player.videoHeight) / 2
-		);
+    canvasContext.drawImage(
+      player,
+      (streamCanvas.width - player.videoWidth) / 2,
+      (streamCanvas.height - player.videoHeight) / 2
+    );
 
-		cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
-		cv.Canny(src, src, 30, 100, 5, false);
-		cv.blur(src, src, ksize, anchor, cv.BORDER_DEFAULT);
-		cv.imshow("edgeDetectionCanvas", src);
-		src.delete();
+    cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
+    cv.Canny(src, src, 30, 100, 5, false);
+    cv.blur(src, src, ksize, anchor, cv.BORDER_DEFAULT);
+    cv.imshow("edgeDetectionCanvas", src);
+    src.delete();
   }, 100);
 };
 
 const canvasModule = async (player) => {
-	var streamCanvas = document.getElementById("streamCanvas");
-	var edgeDetectionCanvas = document.getElementById("edgeDetectionCanvas");
+  var streamCanvas = document.getElementById("streamCanvas");
+  var edgeDetectionCanvas = document.getElementById("edgeDetectionCanvas");
 
-	h = await readLocalStorage("windowHeight");
-	w = await readLocalStorage("windowWidth");
+  h = await readLocalStorage("windowHeight");
+  w = await readLocalStorage("windowWidth");
 
-	streamCanvas.setAttribute("height", h);
-	streamCanvas.setAttribute("width", w);
-	edgeDetectionCanvas.setAttribute("height", h);
-	edgeDetectionCanvas.setAttribute("width", w);
+  streamCanvas.setAttribute("height", h);
+  streamCanvas.setAttribute("width", w);
+  edgeDetectionCanvas.setAttribute("height", h);
+  edgeDetectionCanvas.setAttribute("width", w);
 
-	edgeDetection(player, streamCanvas)
+  edgeDetection(player, streamCanvas);
 };
 
 // ==== AUDIO MODULE ==== //
@@ -84,44 +85,46 @@ gainNode.gain.setValueAtTime(gainNode.gain.value, audioCtx.currentTime);
 
 const setVolume = (param, my) => {
   //set freq corresponding to y cordinate
-  oscillator.frequency.value = initialFreq + 50 * (my / h)
+  oscillator.frequency.value = initialFreq + 50 * (my / h);
   //listen to brightness level as param at cursor position (MAX 255, MIN 0)
-  gainNode.gain.exponentialRampToValueAtTime(param / 100, audioCtx.currentTime + 0.1);
-}
+  gainNode.gain.exponentialRampToValueAtTime(
+    param / 100,
+    audioCtx.currentTime + 0.1
+  );
+};
 
-const getCoordinateData = () => {  
-  chrome.storage.local.get(["mouseX", "mouseY"], function(result) {
-		var mx = result.mouseX;
-		var my = result.mouseY;
-		var output = document.getElementById("edgeDetectionCanvas");
+const getCoordinateData = () => {
+  chrome.storage.local.get(["mouseX", "mouseY"], function (result) {
+    var mx = result.mouseX;
+    var my = result.mouseY;
+    var output = document.getElementById("edgeDetectionCanvas");
 
-		let c = output.getContext("2d");
-		let p = c.getImageData(mx, my, 1, 1).data;
-		let brightness = p[0] ? p[0] : 1;
+    let c = output.getContext("2d");
+    let p = c.getImageData(mx, my, 1, 1).data;
+    let brightness = p[0] ? p[0] : 1;
 
-		xc.innerText = mx.toString();
-		yc.innerText = my.toString();
-		vol.innerText = brightness.toString();
+    xc.innerText = mx.toString();
+    yc.innerText = my.toString();
+    vol.innerText = brightness.toString();
 
-		setVolume(brightness, my);
+    setVolume(brightness, my);
 
-		clearTimeout(timer);
-		timer = setTimeout(() => {
-			setVolume(1, my)
-		}, 1000);
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      setVolume(1, my);
+    }, 1000);
   });
 };
 
 window.addEventListener("load", async (e) => {
-	var player = document.getElementById("player");
+  var player = document.getElementById("player");
 
-	player.srcObject = window.currentStream;
-	player.addEventListener("canplay", async (e) => {
-		e.muted = true;
-		e.play();
-	});
+  player.srcObject = window.currentStream;
+  player.addEventListener("canplay", async (e) => {
+    e.muted = true;
+    e.play();
+  });
 
-	await canvasModule(player);
-	chrome.storage.onChanged.addListener(getCoordinateData);
+  await canvasModule(player);
+  chrome.storage.onChanged.addListener(getCoordinateData);
 });
-  
